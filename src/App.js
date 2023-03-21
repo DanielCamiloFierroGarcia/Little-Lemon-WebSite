6b4b282ea32734bdfc72f1cd42adbdd780872e16
Goldendragonfist <dacafiga1@hotmail.com>
2023-03-21 10:54:23 -0500

    Improve projects structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
+import Header from "./components/header/Header.js";
+import Main from "./components/main/Main.js";
+import Footer from "./components/footer/Footer.js";
+
 function App() {
   return (
     <nav className="App">
-      <Header className="App-header">
-        <div>HomePage</div>
-      </Header>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </nav>
   );
 }
